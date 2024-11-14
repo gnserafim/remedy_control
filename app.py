44925d555ee3ba5e45fcd131cc678b48eb243508
Gabriel Serafim <gnserafim@gmail.com>
2024-11-14 09:46:39 -0300

    Update secrets handling

--- app.py
+++ app.py
@@ -6,9 +6,23 @@ from googleapiclient.http import MediaFileUpload
 
 url = 'https://drive.google.com/uc?id=' + 'https://drive.google.com/file/d/1ZtQB7aJ4Z_0ocg1n9azUM7C3XV-RYoMC/view?usp=drive_link'.split('/')[-2]
 SCOPES = ['https://www.googleapis.com/auth/drive']
-SERVICE_ACCOUNT_FILE = 'remedyupdate2-e2cc4272654b.json'
+#SERVICE_ACCOUNT_FILE = 'remedyupdate2-e2cc4272654b.json'
 PARENT_FOLDER_ID = "1yVLvSlfA830BjWOfCT_OxQguRe1Qj1dD"
 file_id = '1ZtQB7aJ4Z_0ocg1n9azUM7C3XV-RYoMC'
+
+SERVICE_ACCOUNT_FILE = {
+  "type": st.secrets["type"],
+  "project_id": st.secrets["project_id"],
+  "private_key_id": st.secrets["private_key_id"],
+  "private_key": st.secrets["private_key"],
+  "client_email": st.secrets["client_email"],
+  "client_id": st.secrets["client_id"],
+  "auth_uri": st.secrets["auth_uri"],
+  "token_uri": st.secrets["token_uri"],
+  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+  "universe_domain": st.secrets["universe_domain"]
+}
 
 def authenticate():
     creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
