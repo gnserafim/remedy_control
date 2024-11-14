0b9520b7687acb657fdcd9831fc9bc828ba15587
Gabriel Serafim <gnserafim@gmail.com>
2024-11-14 17:36:32 -0300

    final version

--- app.py
+++ app.py
@@ -1,38 +1,36 @@
 import json
 import streamlit as st
 import pandas as pd
+
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
 url = 'https://drive.google.com/uc?id=' + 'https://drive.google.com/file/d/1ZtQB7aJ4Z_0ocg1n9azUM7C3XV-RYoMC/view?usp=drive_link'.split('/')[-2]
 SCOPES = ['https://www.googleapis.com/auth/drive']
-#SERVICE_ACCOUNT_FILE = 'remedyupdate2-e2cc4272654b.json'
 SERVICE_ACCOUNT_FILE = st.secrets['SERVICE_ACCOUNT_FILE']
 PARENT_FOLDER_ID = "1yVLvSlfA830BjWOfCT_OxQguRe1Qj1dD"
-file_id = '1ZtQB7aJ4Z_0ocg1n9azUM7C3XV-RYoMC'
-
-##SERVICE_ACCOUNT_FILE_DICT = {
-  ##"type": st.secrets["type"],
-  #"project_id": st.secrets["project_id"],
-  #"private_key_id": st.secrets["private_key_id"],
-  #"private_key": st.secrets["private_key"],
-  #"client_email": st.secrets["client_email"],
-  #"client_id": st.secrets["client_id"],
-  #"auth_uri": st.secrets["auth_uri"],
-  #"token_uri": st.secrets["token_uri"],
-  #"auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-  #"client_x509_cert_url": st.secrets["client_x509_cert_url"],
-  #"universe_domain": st.secrets["universe_domain"]
-#}
-
-st.write(type(SERVICE_ACCOUNT_FILE))
-
-st.write(SERVICE_ACCOUNT_FILE)
+file_id = '1TqXIyuyL0Knw0Dz1pCH7R_K8MIdpJpp1'
 
 def authenticate():
     creds = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_FILE, scopes=SCOPES) ###  from_service_account_file
     return creds
+
+def load_file(file_id):
+  
+  creds = authenticate()
+  service = build('drive', 'v3', credentials=creds)
+  request = service.files().get_media(fileId=file_id)
+  file_handle = io.BytesIO()
+  downloader = MediaIoBaseDownload(file_handle, request)
+
+  done = False
+  while not done:
+      status, done = downloader.next_chunk()
+
+  file_handle.seek(0)
+
+  return pd.read_csv(file_handle)
 
 def update_file(file_path):
     creds = authenticate()
@@ -55,8 +53,7 @@ def update(edf):
 
 @st.cache_data(ttl='1d')
 def load_df():
-    return pd.read_csv(url)
-
+    return load_file(file_id)
 
 df = load_df()
 edf = st.data_editor(df)
